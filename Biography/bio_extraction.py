6af7ec863617ef6953e78bbb3916c80b8c73eab8
Gurjap Singh <gurjap@uoguelph.ca>
2018-08-29 16:17:08 -0400

    removed commented code from bio_extraciton.py

--- Biography/bio_extraction.py
+++ Biography/bio_extraction.py
@@ -49,7 +49,6 @@ def get_sex(bio):
 def main():
     import os
     filelist = [filename for filename in sorted(os.listdir("/Users/gurjapsingh/Documents/UoGuelph Projects/biography/")) if filename.endswith(".xml")]
-    # filelist.remove("fielmi-b.xml")
     numTriples = 0
     entry_num = 1
     global uber_graph
@@ -62,24 +61,17 @@ def main():
     # 328686 triples created
     # for filename in filelist[:200]:
     # for filename in filelist[:2]:
-    fileo = open("namesAndTriplesNewer.txt","w",encoding="utf-8")
 
     for filename in filelist:
-    # for filename in ["woolvi-b.xml","notlfr-b.xml","atwoma-b.xml","levyam-b.xml"]:
-    # for filename in filelist[1365:]:
-    # for filename in filelist[:25]:
-    # for filename in ["bedfsy-b.xml","blesma-b.xml","butlel-b.xml"]:
-    # for filename in ["delama-b.xml"]:
         with open("/Users/gurjapsingh/Documents/UoGuelph Projects/biography/" + filename,encoding='utf-8') as f:
             soup = BeautifulSoup(f, 'lxml-xml')
 
         print(filename)
         person = Biography(filename[:-6], get_name(soup), cf.get_mapped_term("Gender", get_sex(soup)))
 
-        # education.extract_education_data(soup, person)
-
-        # cf.extract_cf_data(soup, person)
-        # other_contexts.extract_health_contexts_data(soup, person)
+        education.extract_education_data(soup, person)
+        cf.extract_cf_data(soup, person)
+        other_contexts.extract_health_contexts_data(soup, person)
         birthDeath.getBirth(soup,person)
         birthDeath.getDeath(soup,person)
         scrapeFamily.cohabitantsCheck(soup,person)
@@ -88,16 +80,13 @@ def main():
         scrapeFamily.intimateRelationshipsCheck(soup,person)
         scrapeFamily.childlessnessCheck(soup,person)
         scrapeFamily.childrenCheck(soup,person)
-        # location.extract_location_data(soup, person)
-        # other_contexts.extract_other_contexts_data(soup, person)
+        location.extract_location_data(soup, person)
+        other_contexts.extract_other_contexts_data(soup, person)
 
         graph = person.to_graph()
-        graph.remove((person.uri,NS_DICT["foaf"].name,None))
-        graph.remove((person.uri,NS_DICT["foaf"].isPrimaryTopicOf,None))
-        graph.remove((person.uri,NS_DICT["cwrc"].hasGender,None))
-        graph.remove((person.uri,NS_DICT["rdfs"].label,None))
+
         numTriples += len(graph)
-        fileo.write("%s:%d,total:%d\n" % (filename, len(graph),numTriples))
+
         print("length: ",len(graph))
         # Logging bits
         extract_log.subtitle("Entry #" + str(entry_num))
@@ -123,7 +112,7 @@ def main():
         uber_graph += graph
         entry_num += 1
         # exit()
-    fileo.close()
+
 
     turtle_log.subtitle(str(len(uber_graph)) + " triples created")
     turtle_log.msg(uber_graph.serialize(format="ttl").decode(), stdout=False)
