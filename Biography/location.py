fc2d2ba280d4e72934219b36e2431c1d0402f6b3
Alliyya Mo <alliyyamo.dev@gmail.com>
2018-09-19 16:30:49 -0400

    location events + updating cf mapping

--- Biography/location.py
+++ Biography/location.py
@@ -10,13 +10,12 @@ import biography
 from context import Context
 from log import *
 from place import Place
+from event import Event
 
 
 """
-Status: ~55%
+Status: ~80%
 
-Events need to be created--> bigger issue
-Waiting on event modelling and time
 """
 
 # temp log library for debugging --> to be eventually replaced with proper logging library
@@ -32,10 +31,37 @@ uber_graph = rdflib.Graph()
 namespace_manager = rdflib.namespace.NamespaceManager(uber_graph)
 biography.bind_ns(namespace_manager, biography.NS_DICT)
 
+location_occurences = {}
+location_count = {
+    "VISITED": 0,
+    "UNKNOWN": 0,
+    "TRAVELLED": 0,
+    "LIVED": 0,
+    "MIGRATED": 0,
+    "MOVED": 0,
+}
+
+location_event_count = {
+    "VISITED": 0,
+    "UNKNOWN": 0,
+    "TRAVELLED": 0,
+    "LIVED": 0,
+    "MIGRATED": 0,
+    "MOVED": 0,
+}
+
 
 class Location(object):
     """docstring for Location
     """
+    location_dict = {
+        "VISITED": "visits",
+        "UNKNOWN": "relatesSpatiallyTo",
+        "TRAVELLED": "travelsTo",
+        "LIVED": "inhabits",
+        "MIGRATED": "migratesTo",
+        "MOVED": "relocatesTo",
+    }
 
     def __init__(self, predicate, place, other_attributes=None):
         super(Location, self).__init__()
@@ -67,84 +93,141 @@ class Location(object):
         return string
 
 
-def get_reg(tag):
-    return get_attribute(tag, "REG")
+def check_occurence(place_uri):
+    global location_occurences
+    for x in location_occurences.keys():
+        if place_uri in location_occurences[x]:
+            return True
+
+    return False
 
 
-def get_attribute(tag, attribute):
-    value = tag.get(attribute)
-    if value:
-        return value
-    return None
+def find_locations(tag, relation):
+    """Creates location list given the tag
+    """
+    global location_occurences
+    location_list = []
+    location_type = relation
+    places = tag.find_all("PLACE")
+
+    if location_type in ["VISITED", "UNKNOWN", "TRAVELLED", "LIVED"]:
+        predicate = Location.location_dict[location_type]
+        for place in places:
+            location_list.append(Location(predicate, place))
+    elif location_type == "MOVED":
+        if len(places) > 1:
+            for place in places:
+                if "leaving " + place.text in tag.text or "left " + place.text in tag.text:
+                    place_uri = Place(place).uri
+                    location_occurences["MOVED"].remove(place_uri)
+                    if not check_occurence(place_uri):
+                        location_list.append(Location("relatesSpatiallyTo", place))
+                    location_occurences["MOVED"].append(place_uri)
+                else:
+                    location_list.append(Location("relocatesTo", place))
+
+        else:
+            for place in places:
+                location_list.append(Location("relocatesTo", place))
+    elif location_type == "MIGRATED":
+        if len(places) == 1:
+            location_list.append(Location("migratesTo", places[0]))
+        else:
+            for place in places:
+                # TODO: clean up these if statements
+                if "leaving " + place.text in tag.text or "left " + place.text in tag.text or "from " + place.text in tag.text:
+                    location_list.append(Location("migratesFrom", place))
+                elif "to " + place.text in tag.text or "to the " + place.text in tag.text or "at " + place.text in tag.text:
+                    location_list.append(Location("migratesTo", place))
+
+    return location_list
 
 
-def get_value(tag):
-    value = get_reg(tag)
-    if not value:
-        value = get_attribute(tag, "CURRENTALTERNATIVETERM")
-    if not value:
-        value = str(tag.text)
-        value = ' '.join(value.split())
-    return value
+def get_place_occurences(locations):
+    """Gets all the places associated with the different placec tags
+    """
+    location_occurences = {
+        "VISITED": [],
+        "UNKNOWN": [],
+        "TRAVELLED": [],
+        "LIVED": [],
+        "MIGRATED": [],
+        "MOVED": [],
+    }
+
+    for location in locations:
+        places = location.find_all("PLACE")
+        location_occurences[location.get("RELATIONTO")] += ([Place(x).uri for x in places])
+    return location_occurences
+
+
+def extract_locations(tag_list, context_type, person, list_type="paragraphs"):
+    """ Creates the location relation and ascribes them to the person along with the associated
+        contexts and event
+    """
+    global location_count
+    global location_event_count
+
+    for tag in tag_list:
+        temp_context = None
+        location_list = None
+        context_id = person.id + "_SpatialContext" + "-" + str(Location.location_dict[context_type])
+        location_count[context_type] += 1
+        context_id += "_" + str(location_count[context_type])
+
+        location_list = find_locations(tag, context_type)
+        if location_list:
+            temp_context = Context(context_id, tag, "SpatialContext")
+            temp_context.link_triples(location_list)
+            person.add_location(location_list)
+        else:
+            temp_context = Context(context_id, tag, "SpatialContext", "identifying")
+
+        if list_type == "events":
+            location_event_count[context_type] += 1
+            event_title = person.name + " - " + "Spatial (" + Location.location_dict[context_type] + ") Event"
+            event_uri = person.id + "_" + \
+                Location.location_dict[context_type] + "_Event" + str(location_event_count[context_type])
+            temp_event = Event(event_title, event_uri, tag)
+            temp_context.link_event(temp_event)
+            person.add_event(temp_event)
+
+        person.add_context(temp_context)
 
 
 def extract_location_data(bio, person):
-    """
-    Notes: Lived and Moved have instructions that depend on the presence of locations in
-    other tags, may have to seek clarification
-    TODO: 
-    1) investigate "LIVED" with multiple places and common words preceding and following places
-    2) same for moved
-    3) Migrated as well
-
-    Need to be revised with events
-
-    """
     locations = bio.find_all("LOCATION")
-    location_dict = {
-        "VISITED": "visits",
-        "UNKNOWN": "relatesSpatiallyTo",
-        "TRAVELLED": "travelsTo",
-        "LIVED": "inhabits",
-        "MIGRATED": "migratesTo",
-        "MOVED": "relocatesTo",
+    global location_occurences
+    global location_count
+    global location_event_count
+    location_occurences = get_place_occurences(locations)
+    location_count = {
+        "VISITED": 0,
+        "UNKNOWN": 0,
+        "TRAVELLED": 0,
+        "LIVED": 0,
+        "MIGRATED": 0,
+        "MOVED": 0,
     }
-
-    id = 1
+    location_event_count = {
+        "VISITED": 0,
+        "UNKNOWN": 0,
+        "TRAVELLED": 0,
+        "LIVED": 0,
+        "MIGRATED": 0,
+        "MOVED": 0,
+    }
+    # id = 1
     for location in locations:
-        location_list = []
+        # forms_found = 0
         location_type = location.get("RELATIONTO")
-        places = location.find_all("PLACE")
-        context_id = person.id + "_SpatialContext_" + location_dict[location_type] + "_" + str(id)
 
-        if len(places) == 0:
-            temp_context = Context(context_id, location, "LOCATION", "identifying")
-            person.add_context(temp_context)
-            continue
+        paragraphs = location.find_all("P")
+        events = location.find_all("CHRONSTRUCT")
+        extract_locations(paragraphs, location_type, person)
+        extract_locations(events, location_type, person, "events")
 
-        elif location_type in ["VISITED", "UNKNOWN", "TRAVELLED"]:
-            predicate = location_dict[location_type]
-            for place in places:
-                location_list.append(Location(predicate, place))
-        elif location_type == "LIVED":
-            print(len(places))
-            if len(places) > 1:
-                log.msg(person.id)
-                log.msg(str(location))
-                log.msg(location_type)
-            elif len(places) == 1:
-                location_list.append(Location("inhabits", places[0]))
-
-        elif location_type == "MOVED":
-            continue
-        elif location_type == "MIGRATED":
-            continue
-
-        temp_context = Context(context_id, location, "location")
-        temp_context.link_triples(location_list)
-        person.add_location(location_list)
-        person.add_context(temp_context)
-        id += 1
+        # location_count[location_type] += forms_found
 
 
 def main():
@@ -185,6 +268,11 @@ def main():
         extract_log.msg(test_person.to_file(graph))
         extract_log.subtitle("Entry #" + str(entry_num))
         extract_log.msg("\n\n")
+
+        file = open("location_turtle/" + filename[:-6] + "_location.ttl", "w", encoding="utf-8")
+        file.write("#" + str(len(graph)) + " triples created\n")
+        file.write(graph.serialize(format="ttl").decode())
+        file.close()
 
         uber_graph += graph
         entry_num += 1
