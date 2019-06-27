52bdc318b92d126d355e88f473fedfd5ea7d22dd
Alliyya Mo <alliyyamo.dev@gmail.com>
2019-06-27 14:21:13 -0400

    Revision of context model for context centric

--- Utils/context.py
+++ Utils/context.py
@@ -11,16 +11,40 @@ logger = utilities.config_logger("context")
 """
 Status: ~84%
 TODO:
-1) Revise mechanism for adding triples as a texual body for less list oriented components ex. Death
-2) revise mechanism for getting closest heading
-3) Fix up labelling of contexts possibly
-4) Revise text snippet to grab from where the first triple is extracted
-    - however sometime for identifying contexts, names/orgs are identified
-        prior to triples extracted
+
+1) revise mechanism for getting closest heading
+2) Fix up labelling of contexts possibly
+3) replace mapping related fx with a closure
+4) clean up imports
 """
 
 
-def identifying_motivation(tag):
+def get_xpath(element):
+    """
+    Generate xpath from BeautifulSoup4 element
+    :param element: BeautifulSoup4 element.
+    :type element: bs4.element.Tag or bs4.element.NavigableString
+    :return: xpath as string
+    :rtype: str
+    """
+    components = []
+    child = element if element.name else element.parent
+    for parent in child.parents:
+        """
+        @type parent: bs4.element.Tag
+        """
+        siblings = parent.find_all(child.name, recursive=False)
+        components.append(
+            child.name
+            if siblings == [child] else
+            '%s[%d]' % (child.name, 1 + siblings.index(child))
+        )
+        child = parent
+    components.reverse()
+    return '/%s' % '/'.join(components)
+
+
+def get_named_entities(tag):
     """ extracts the identifying components in a given tag
         to be used for the subjects of the annotation
     """
@@ -52,6 +76,50 @@ def get_heading(tag):
     return utilities.remove_punctuation(utilities.strip_all_whitespace(heading.text), True)
 
 
+def create_context_map():
+
+    import pandas as pd
+    with open('../data/context_mapping.csv', newline='') as csvfile:
+        return pd.read_csv(csvfile)
+
+
+def get_context_map_res(col, tag, mode=False):
+    # will need to revise should contexts have the same mode attribute
+    if mode:
+        index = Context.MAPPING[Context.MAPPING['Mode'] == tag].index[0]
+    else:
+        index = Context.MAPPING[Context.MAPPING['Orlando Tag'] == tag].index[0]
+    return Context.MAPPING[col][index]
+
+
+def get_context_predicate(tag, mode=None):
+    if mode:
+        return get_context_map_res("Context relationship predicate", mode, True)
+    return get_context_map_res("Context relationship predicate", tag)
+
+
+def get_event_type(tag, mode=None):
+    if mode:
+        return get_context_map_res("Event", mode, True)
+    return get_context_map_res("Event", tag)
+
+
+def get_context_type(tag, mode=None):
+    if mode:
+        return get_context_map_res("Context", mode, True)
+    return get_context_map_res("Context", tag)
+
+
+def remove_unwanted_tags(tag):
+    unwanted_tag_names = ["BIBCITS", "RESPONSIBILITIES", "KEYWORDCLASSES"]
+    unwanted_tags = []
+    for x in unwanted_tag_names:
+        unwanted_tags += tag.find_all(x)
+
+    for x in unwanted_tags:
+        x.decompose()
+
+
 class Context(object):
     """
     given the id for creating a context, the tag, context_type
@@ -62,100 +130,47 @@ class Context(object):
     motivation="identifying" as argument is necessary
     # TODO: Create possible subclass for necessary other motivations or
     + logic depending on complexity
-    """
-    """
-    Could put this info in a csv --> to map
-    Orlando tag | context | context relationship predicate
-    Possibly add mode since mode may be a modifying attribute
-    Extend this events quite possibly?
-    Orlando tag | mode  | context | context relationship predicate | event name
 
+    TODO: Possibly move out this mapping to utilities for less coupling
+    TODO: review better way to generate of ID of context
     """
-    context_types = ["BiographyContext", "BirthContext", "CulturalFormContext",
-                     "DeathContext", "FamilyContext", "FriendsAndAssociatesContext",
-                     "GenderContext", "IntimateRelationshipContext", "LeisureContext",
-                     "NationalityContext", "OccupationContext", "PoliticalContext",
-                     "RaceEthnicityContext", "ReligionContext", "SexualityContext",
-                     "SocialClassContext", "SpatialContext", "ViolenceContext",
-                     "EconomicContext", "EducationContext", "NameContext", "Context"
-                     "InstitutionalEducationContext", "SelfTaughtEducationContext", "DomesticEducationContext"]
+    MAPPING = create_context_map()
 
-    context_map = {"CLASSISSUE": "SocialClassContext",
-                   "RACEANDETHNICITY": "RaceEthnicityContext",
-                   "NATIONALITYISSUE": "NationalityContext",
-                   "SEXUALITY": "SexualityContext",
-                   "POLITICS": "PoliticalContext",
-                   "RELIGION": "ReligionContext",
-                   "CULTURALFORMATION": "CulturalFormContext",
-                   "LEISUREANDSOCIETY": "LeisureContext",
-                   "OCCUPATION": "OccupationContext",
-                   "LOCATION": "SpatialContext",
-                   "VIOLENCE": "ViolenceContext",
-                   "WEALTH": "EconomicContext",
-                   "OTHERLIFEEVENT": "BiographyContext",
-                   "FAMILY": "FamilyContext",
-                   "BIRTH": "BirthContext",
-                   "DEATH": "DeathContext",
-                   "FRIENDSASSOCIATES": "FriendsAndAssociatesContext",
-                   "INTIMATERELATIONSHIPS": "IntimateRelationshipContext",
-                   "PERSONNAME": "NameContext",
-                   None: "Context"
-                   }
-    # context->predicate
-    predicate_map = {
-        "SocialClassContext": "culturalFormRelationship",
-        "RaceEthnicityContext": "culturalFormRelationship",
-        "NationalityContext": "culturalFormRelationship",
-        "SexualityContext": "culturalFormRelationship",
-        "PoliticalContext": "culturalFormRelationship",
-        "ReligionContext": "culturalFormRelationship",
-        "CulturalFormContext": "culturalFormRelationship",
-        "LeisureContext": "leisureRelationship",
-        "OccupationContext": "occupationRelationship",
-        "SpatialContext": "spatialRelationship",
-        "ViolenceContext": "violenceAssociation",
-        "EconomicContext": "economicRelationship",
-        "BiographyContext": "biographyRelationship",
-        "FamilyContext": "familyRelationship",
-        "BirthContext": "birthRelationship",
-        "DeathContext": "deathRelationship",
-        "FriendsAndAssociatesContext": "friendsAndAssociatesRelationship",
-        "IntimateRelationshipContext": "intimateRelationshipRelationship",
-        "NameContext": "nameRelationship"
-    }
-
-    def __init__(self, id, tag, context_type="CULTURALFORMATION", motivation="describing"):
+    def __init__(self, id, tag, context_type="CULTURALFORMATION", motivation="describing", mode=None):
         super(Context, self).__init__()
         self.id = id
         self.triples = []
         self.event = None
+        self.xpath = get_xpath(tag)
 
-        unwanted_tags = tag.find_all("BIBCITS") + tag.find_all("RESPONSIBILITIES") + tag.find_all("KEYWORDCLASSES")
-        for x in unwanted_tags:
-            x.decompose()
-
+        remove_unwanted_tags(tag)
         self.tag = tag
+
         self.heading = get_heading(tag)
         self.src = "http://orlando.cambridge.org/protected/svPeople?formname=r&people_tab=3&person_id="
         if not self.heading:
             self.src = "http://orlando.cambridge.org"
 
-        # TODO: Make snippet start where nearest period to 35 words . . . .
+        # TODO: Make snippet endwhere nearest period to 35 words . . . .
         # Making the text the max amount of words
         self.text = utilities.limit_words(str(tag.get_text()), MAX_WORD_COUNT)
 
         # Would be nice to use the ontology and not worry about changing labels
-        if context_type in self.context_map:
-            self.context_type = utilities.create_uri("cwrc", self.context_map[context_type])
-            self.context_label = utilities.split_by_casing(self.context_map[context_type])
-        else:
-            self.context_type = utilities.create_uri("cwrc", context_type)
-            self.context_label = utilities.split_by_casing(context_type)
+        # print(context_type, mode)
+        self.context_type = get_context_type(context_type, mode)
+        self.context_label = utilities.split_by_casing(self.context_type)
+        self.context_type = utilities.create_cwrc_uri(self.context_type)
+
+        self.named_entities = get_named_entities(self.tag)
+        self.context_predicate = utilities.create_cwrc_uri(get_context_predicate(context_type))
+
+        if self.named_entities:
+            motivation = "describing"
+
+        if motivation == "identifying":
+            self.subjects = get_named_entities(self.tag)
 
         self.motivation = utilities.create_uri("oa", motivation)
-        self.subjects = []
-        if motivation == "identifying":
-            self.subjects = identifying_motivation(self.tag)
         self.uri = utilities.create_uri("data", id)
 
     def link_triples(self, comp_list):
@@ -191,21 +206,33 @@ class Context(object):
         # if tag is a describing None create the identifying triples
         g = utilities.create_graph()
 
-        # Creating Textual body first
-        snippet_uri = rdflib.term.URIRef(str(self.uri) + "_Snippet")
-
+        # Creating target first
+        target_uri = utilities.create_uri("data", self.id + "_Target")
         if person:
             source_url = rdflib.term.URIRef(self.src + person.id + "#" + self.heading)
-            snippet_label = person.name + " - " + self.context_label + " snippet"
+            target_label = person.name + " - " + self.context_label + " target"
         else:
             source_url = rdflib.term.URIRef(self.src + "#FE")
-            snippet_label = "FE" + " - " + self.context_label + " snippet"
+            target_label = "FE" + " - " + self.context_label + " target"
 
-        g.add((snippet_uri, RDF.type, utilities.NS_DICT["oa"].TextualBody))
-        g.add((snippet_uri, RDFS.label, rdflib.term.Literal(snippet_label)))
-        g.add((snippet_uri, utilities.NS_DICT["oa"].hasSource, source_url))
-        g.add((snippet_uri, utilities.NS_DICT["dcterms"].description, rdflib.term.Literal(
-            self.text, datatype=rdflib.namespace.XSD.string)))
+        g.add((target_uri, RDFS.label, rdflib.term.Literal(target_label)))
+        g.add((target_uri, utilities.NS_DICT["oa"].hasSource, source_url))
+
+        # Creating xpath selector
+        xpath_uri = utilities.create_uri("data", self.id + "_XPath")
+        xpath_label = target_label.replace(" target", " XPath Selector")
+        g.add((target_uri, utilities.NS_DICT["oa"].hasSelector, xpath_uri))
+        g.add((xpath_uri, RDFS.label, rdflib.term.Literal(xpath_label)))
+        g.add((xpath_uri, RDF.type, utilities.NS_DICT["oa"].XPathSelector))
+        g.add((xpath_uri, RDF.value, rdflib.term.Literal(self.xpath)))
+
+        # Creating text quote selector
+        textquote_uri = utilities.create_uri("data", self.id + "_TextquoteSelector")
+        textquote_label = target_label.replace(" target", " TextQuote Selector")
+        g.add((xpath_uri, utilities.NS_DICT["oa"].refinedBy, textquote_uri))
+        g.add((textquote_uri, RDF.type, utilities.NS_DICT["oa"].TextQuoteSelector))
+        g.add((textquote_uri, RDFS.label, rdflib.term.Literal(textquote_label)))
+        g.add((textquote_uri, utilities.NS_DICT["oa"].exact, rdflib.term.Literal(self.text)))
 
         # Creating identifying context first and always
         if person:
@@ -216,16 +243,15 @@ class Context(object):
         identifying_uri = utilities.create_uri("data", self.id + "_identifying")
         g.add((identifying_uri, RDF.type, self.context_type))
         g.add((identifying_uri, RDFS.label, rdflib.term.Literal(context_label)))
-        g.add((identifying_uri, utilities.NS_DICT["oa"].hasTarget, snippet_uri))
+        g.add((identifying_uri, utilities.NS_DICT["oa"].hasTarget, target_uri))
         g.add((identifying_uri, utilities.NS_DICT["oa"].motivatedBy, utilities.NS_DICT["oa"].identifying))
-        self.subjects += identifying_motivation(self.tag)
-        if self.triples and person:
-            self.subjects += self.get_subjects(self.triples, person)
-        for x in self.subjects:
+
+        for x in self.named_entities:
             g.add((identifying_uri, utilities.NS_DICT["oa"].hasBody, x))
 
         if person:
             g.add((identifying_uri, utilities.NS_DICT["oa"].hasBody, person.uri))
+            # g.add((identifying_uri, utilities.NS_DICT["cwrc"].contextFocus, person.uri))
 
         if self.event:
             g.add((identifying_uri, utilities.NS_DICT["cwrc"].hasEvent, self.event))
@@ -237,37 +263,28 @@ class Context(object):
             g.add((self.uri, RDF.type, self.context_type))
             g.add((self.uri, RDFS.label, rdflib.term.Literal(context_label)))
             g.add((self.uri, utilities.NS_DICT["cwrc"].hasIDependencyOn, identifying_uri))
-            g.add((self.uri, utilities.NS_DICT["oa"].hasTarget, person.uri))
-            g.add((self.uri, utilities.NS_DICT["oa"].hasTarget, snippet_uri))
+            g.add((self.uri, utilities.NS_DICT["cwrc"].contextFocus, person.uri))
+            g.add((self.uri, utilities.NS_DICT["oa"].hasTarget, target_uri))
             g.add((self.uri, utilities.NS_DICT["oa"].motivatedBy, self.motivation))
 
-            for x in self.subjects:
-                g.add((self.uri, utilities.NS_DICT["dcterms"].subject, x))
-
             for x in self.triples:
-                temp_str = x.to_triple(person).serialize(format="ttl").decode().splitlines()
-                triple_str_test = [y for y in temp_str if "@prefix" not in y and y != '']
-                if len(triple_str_test) == 1:
-                    triple_str = x.to_triple(person).serialize(format="ttl").decode().splitlines()[-2]
-                    g += self.create_ttl_body(triple_str)
-                else:
-                    triple_str = "\n".join(triple_str_test)
-                    g += self.create_multiple_triples(x.to_triple(person))
+                g += x.to_triple(self)
 
             if self.event:
                 g.add((self.uri, utilities.NS_DICT["cwrc"].hasEvent, self.event))
-                g.add((self.event, utilities.NS_DICT["cwrc"].hasContext, self.uri))
+
+            for x in self.named_entities:
+                g.add((self.uri, self.context_predicate, x))
 
         # Creating the mentioned people as natural person
         for x in self.tag.find_all("NAME"):
             uri = utilities.make_standard_uri(x.get("STANDARD"))
             g.add((uri, RDF.type, utilities.NS_DICT["cwrc"].NaturalPerson))
             g.add((uri, RDFS.label, Literal(x.get("STANDARD"), datatype=rdflib.namespace.XSD.string)))
-            g.add((uri, utilities.NS_DICT["foaf"].name, Literal(
-                x.get("STANDARD"), datatype=rdflib.namespace.XSD.string)))
 
         return g
 
+# TODO: Remove these functions
     def create_multiple_triples(self, graph):
         """handles multitple triples
         """
