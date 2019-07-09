e9aa6a4939e3e121d283762bf04322846ad38ac3
Alliyya Mo <alliyyamo.dev@gmail.com>
2019-07-09 11:59:29 -0400

    updating intimate relationships to include events and attach to the context

--- Biography/lifeInfo.py
+++ Biography/lifeInfo.py
@@ -18,16 +18,22 @@ class Person(object):
 
     def __init__(self, name_tag, relationship, other_attributes=None):
         super(Person, self).__init__()
-        self.name = name_tag.get("STANDARD")
-        self.uri = utilities.get_name_uri(name_tag)
+        if other_attributes:
+            self.name = None
+            self.uri = name_tag
+        else:
+            self.name = name_tag.get("STANDARD")
+            self.uri = utilities.get_name_uri(name_tag)
+
         # TODO possibly mapping to get relationship
         self.predicate = utilities.create_cwrc_uri(relationship)
 
     def to_triple(self, context):
         g = utilities.create_graph()
-        g.add((self.uri, RDFS.label, Literal(self.name)))
         g.add((self.uri, RDF.type, utilities.create_cwrc_uri("NaturalPerson")))
         g.add((context.uri, self.predicate, self.uri))
+        if self.name:
+            g.add((self.uri, RDFS.label, Literal(self.name)))
         return g
 
     def __str__(self):
@@ -37,13 +43,6 @@ class Person(object):
         string += "\tvalue: " + str(self.value) + "\n"
 
         return string
-
-
-# Get g information about the subject
-# ------ Example ------
-# birth date:  1873-12-07
-# birth positions: ['ELDEST']
-# birth place: Gore, Virginia, USA
 
 
 # checks if child's name is available in children tag
@@ -131,115 +130,79 @@ def extract_cohabitants(xmlString, person):
     person.cohabitants_list = listToReturn
 
 
-def getSexualityContexts(xmlString):
-    root = xmlString.BIOGRAPHY
-    tagToFind = allTagsAllChildren(root, "SEXUALITY")
-    # tagToFind = root.find_all("SEXUALITY").find_all(recursive=False)
-    listToReturn = []
+def find_relationships(tag, person, relation):
+    predicate_map = {
+        "EROTICYES": "eroticRelationship",
+        "EROTICPOSSIBLY": "possiblyEroticRelationship",
+        "EROTICNO": "nonEroticRelationship",
+        None: "intimateRelationship"
+    }
+    relationships = []
+    if relation is None:
+        people_found = utilities.get_people(tag)
+        if person.uri in people_found:
+            people_found.remove(person.uri)
+        if len(people_found) == 1:
+            logger.info(str(people_found[0]) + "-->" + str(relation) + " of " + person.name)
+            relationships.append(Person(people_found[0], predicate_map[relation], True))
 
-    for div in tagToFind:
-        # print(div.findall("*"))
-        sexualityContext = getContexts(div)
-        listToReturn += sexualityContext
-    print(listToReturn)
-#    ================================================
-#     root = BeautifulSoup(xmlString,'lxml')
-#     print(root)
-    return listToReturn
+    else:
+        relationships = find_friends(tag, person, predicate_map[relation])
+
+    return relationships
 
 
-def extract_intimate_relationships(xmlString, person):
-    root = xmlString.BIOGRAPHY
-    irTag = root.find_all('INTIMATERELATIONSHIPS')
-    intimateRelationships = []
-    sourcePerson = person.name
-    id = 1
-    for tag in irTag:
-        attr = ""
+def extract_relationships(tag_list, context_type, person, list_type="paragraphs"):
+    """ Creates the interpersonal relation and ascribes them to the person along
+        with the associated contexts and event
+    """
+    global context_count
+    global event_count
+    CONTEXT_TYPE = get_context_type("INTIMATERELATIONSHIPS")
+    EVENT_TYPE = get_event_type("INTIMATERELATIONSHIPS")
 
-        if "EROTIC" in tag.attrs:
-            attr = tag["EROTIC"]
-            # print("attr: ", tag.attrib["EROTIC"])
+    for tag in tag_list:
+        temp_context = None
+        relationship_list = None
+        context_count += 1
+        context_id = person.id + "_" + CONTEXT_TYPE + "_" + str(context_count)
+        relationship_list = find_relationships(tag, person, context_type)
+        if relationship_list:
+            temp_context = Context(context_id, tag, "INTIMATERELATIONSHIPS")
+            temp_context.link_triples(relationship_list)
         else:
-            attr = "nonErotic"
-        for thisPerson in tag.find_all("DIV2"):
-            print("======person======")
-            print("source: ", sourcePerson)
+            temp_context = Context(context_id, tag, "INTIMATERELATIONSHIPS", "identifying")
 
-            context_id = person.id + "_IntimateRelationshipsContext_" + str(id)
-            id += 1
-            names = getAllNames(thisPerson.find_all("NAME"), person.name)
-            if len(names) >= 1:
-                print("========>", names[0])
-                thisRelationship = IntimateRelationships(names[0], attr)
-            else:
-                thisRelationship = IntimateRelationships("intimate relationship", attr)
+        if list_type == "events":
+            event_count += 1
+            event_title = person.name + " - " + "Intimate Relationship Event"
+            event_uri = person.id + "_IntimateRelationshipEvent_" + str(event_count)
+            temp_event = Event(event_title, event_uri, tag, EVENT_TYPE)
+            temp_context.link_event(temp_event)
+            person.add_event(temp_event)
 
-            tempContext = Context(context_id, thisPerson, "INTIMATERELATIONSHIPS")
-            tempContext.link_triples([thisRelationship])
-            intimateRelationships.append(thisRelationship)
-            person.context_list.append(tempContext)
-            # for name in person.iter("NAME"):
-            #     print(name.attrib["STANDARD"])
-            # getch()
-    # intimateRelationships = IntimateRelationships(personAttrList,intimateContexts)
-
-    person.intimateRelationships_list = intimateRelationships
-
-# This function obtains family information
-# ------ Example ------
-# Name:  Grant, Josceline Charles Henry
-# Relation:  FATHER
-# Jobs: army officer
-# SigAct: lost money, currency committee
+        person.add_context(temp_context)
 
 
-def extract_family(xmlString, person):
-
-    myRoot2 = xmlString.BIOGRAPHY
-    # SOURCENAME = myRoot2.newFindFunc("DIV0 STANDARD").text
-    SOURCENAME = findTag(myRoot2, "DIV0 STANDARD").text
-    listOfMembers = []
-    fams = myRoot2.find_all('FAMILY')
-    for familyTag in myRoot2.find_all('FAMILY'):
-
-        #--------------------------------- Get husband and wife ---------------------------------
-        for familyMember in familyTag.find_all("MEMBER"):
-            if familyMember['RELATION'] in ["HUSBAND", "WIFE"]:
-                if len(familyMember.find_all()) == 1:
-                    continue
-                else:
-                    listOfMembers = getMemberInfo(familyMember, listOfMembers, SOURCENAME)
-
-        #--------------------------------- get children ---------------------------------
-        for familyMember in familyTag.find_all("MEMBER"):
-            if familyMember['RELATION'] in ["SON", "DAUGHTER", "STEPSON", "STEPDAUGHTER"]:
-                if len(familyMember.find_all()) == 1:
-                    continue
-                else:
-                    listOfMembers = getMemberChildInfo(familyMember, listOfMembers, SOURCENAME)
-
-        #--------------------------------- get others ---------------------------------
-        for familyMember in familyTag.find_all('MEMBER'):
-            finds = familyMember.find_all()
-            if familyMember['RELATION'] in ["HUSBAND", "WIFE", "SON", "DAUGHTER", "STEPSON", "STEPDAUGHTER"] or len(iterListAll(familyMember)) == 1:
-                continue
-            else:
-                listOfMembers = getMemberInfo(familyMember, listOfMembers, SOURCENAME)
-
-    print("----------- ", SOURCENAME.strip(), "'s Family Members -----------")
-    # printMemberInfo(listOfMembers)
-    # print("")
-    # return rearrangeSourceName(SOURCENAME),listOfMembers
-    # return SOURCENAME,listOfMembers
-    person.family_list = listOfMembers
+def extract_intimate_relationships_data(bio, person):
+    relationship_tags = bio.find_all('INTIMATERELATIONSHIPS')
+    global context_count
+    global event_count
+    context_count = 0
+    event_count = 0
+    for tag in relationship_tags:
+        relation = tag.get("EROTIC")
+        paragraphs = tag.find_all("P")
+        events = tag.find_all("CHRONSTRUCT")
+        extract_relationships(paragraphs, relation, person)
+        extract_relationships(events, relation, person, "events")
 
 
-def find_friends(tag, person):
+def find_friends(tag, person, predicate="interpersonalRelationshipWith"):
     friends = []
     names = tag.find_all("NAME")
     for x in names:
-        friends.append(Person(x, "interpersonalRelationshipWith"))
+        friends.append(Person(x, predicate))
     return list(filter(lambda a: a.uri != person.uri, friends))
 
 
@@ -288,13 +251,77 @@ def extract_friend_data(bio, person):
         extract_friends(events, "FRIENDSASSOCIATES", person, "events")
 
 
+def create_family_map(path=None):
+    # Create better searching mechanism
+    if not path:
+        path = '../data/family_mapping.csv'
+    with open(path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        for row in reader:
+            if row[0] not in FAMILY_MAP:
+                FAMILY_MAP[row[0]] = row[1]
+
+FAMILY_MAP = {}
+create_family_map()
+
+
 def extract_family_data(bio, person):
-    family_members = []
-    # for tag in bio.find_all('FAMILY'):
-        # for member in tag.find_all("MEMBER"):
-    # Find all husband wife tags
-    # children
-    # other members
+    # TODO: create duplicate contexts implying inverse operations
+    # Acquire occupations
+    # TODO: Extract family members in a certain orders
+    context_count = 1
+    event_count = 1
+    family_tags = bio.find_all("FAMILY")
+
+    for family_tag in family_tags:
+        member_tags = family_tag.find_all("MEMBER")
+
+        for member_tag in member_tags:
+            family_members = []
+            relation = FAMILY_MAP[member_tag["RELATION"]]
+            context_id = person.id + "_FamilyContext_" + str(context_count)
+            temp_context = Context(context_id, member_tag, "FAMILY")
+
+            # Finding family member
+            people_found = utilities.get_people(member_tag)
+            if person.uri in people_found:
+                people_found.remove(person.uri)
+            if len(people_found) == 1:
+                logger.info(str(people_found[0]) + "-->" + relation + " of " + person.name)
+                family_members.append(Person(people_found[0], relation, True))
+
+            # Creating family events
+            events_tags = member_tag.find_all("CHRONSTRUCT")
+            family_events = []
+            for x in events_tags:
+                event_title = person.name + " - Family Event (" + relation + ")"
+                event_uri = person.id + "_FamilyEvent_" + str(event_count)
+                family_events.append(Event(event_title, event_uri, x, "FamilyEvent"))
+                event_count += 1
+
+            temp_context.link_triples(family_members)
+            for x in family_events:
+                temp_context.link_event(x)
+                person.add_event(x)
+
+            person.add_context(temp_context)
+            context_count += 1
+
+        if len(member_tags) == 0:
+            context_id = person.id + "_FamilyContext_" + str(context_count)
+            temp_context = Context(context_id, family_tag, "FAMILY")
+            events_tags = family_tag.find_all("CHRONSTRUCT")
+            family_events = []
+
+            for x in events_tags:
+                event_title = person.name + " - Family Event"
+                event_uri = person.id + "_FamilyEvent_" + str(event_count)
+                family_events.append(Event(event_title, event_uri, x, "FamilyEvent"))
+                event_count += 1
+
+            person.add_context(temp_context)
+            context_count += 1
 
 
 def main():
@@ -320,10 +347,11 @@ def main():
 
         person = Biography(person_id, soup, culturalForm.get_mapped_term("Gender", utilities.get_sex(soup)))
         extract_friend_data(soup, person)
+        # bagnen
+        extract_family_data(soup, person)
+        extract_intimate_relationships_data(soup, person)
+
         # extract_cohabitants(soup, person)
-        # extract_family(soup, person)
-        # extract_friends_associates(soup, person)
-        # extract_intimate_relationships(soup, person)
         # extract_childlessness(soup, person)
         # extract_children(soup, person)
 
