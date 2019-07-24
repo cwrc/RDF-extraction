e6ba169135bafbf3522525fae04da762a34da3a1
Alliyya Mo <alliyyamo.dev@gmail.com>
2019-07-24 15:56:51 -0400

    Adding logging for citations

--- Utils/citation.py
+++ Utils/citation.py
@@ -1,15 +1,21 @@
 from rdflib import RDF, RDFS, Literal, BNode
 from Utils import utilities
 
+logger = utilities.config_logger("citation")
+
 
 class Citation(object):
     """docstring for Citation"""
 
     def __init__(self, bibcit_tag):
         super(Citation, self).__init__()
+        self.tag = bibcit_tag
         self.page = bibcit_tag.text
         self.label = bibcit_tag.get("PLACEHOLDER")
         self.citing_entity = bibcit_tag.get("DBREF")
+        #NOTE: Temporarily logging this info for decision about page numbers
+        if "," in self.page or "-" in self.page:
+            logger.info("\n\t" + str(self.tag) + "\n")
 
     def to_triple(self, target_uri, source_url=None):
         g = utilities.create_graph()
@@ -31,7 +37,8 @@ class Citation(object):
         return g
 
     def __str__(self):
-        string = "Label: " + self.label + "\n"
+        string = "Tag: " + str(self.tag) + "\n"
+        string += "Label: " + self.label + "\n"
         string += "Page: " + self.page + "\n"
-        string += "citing_entity: " + self.citing_entity + "\n"
+        string += "Citing entity: " + self.citing_entity + "\n"
         return string
