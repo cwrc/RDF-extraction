fb2bd9ffb6a14d55bd434783ee3c38c185d0bffc
Alliyya Mo <alliyyamo.dev@gmail.com>
2022-03-04 08:20:28 -0500

    adding other_contexts to be extracted to driver

--- Biography/bio_extraction.py
+++ Biography/bio_extraction.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 from bs4 import BeautifulSoup
-
 from Utils import utilities, place, organizations
 from biography import Biography
 import culturalForm as cf
 import location
-import other_contexts
-import occupation
-import lifeInfo
 import birthDeath
+import occupation
 
+import other_contexts
+import lifeInfo
 import education
 import personname
 
@@ -32,18 +31,15 @@ def main():
     least_triples = 0
     smallest_person = None
     largest_person = None
+    total_triples = 0
     logger.info("Time started: " + utilities.get_current_time() + "\n")
 
     for filename in file_dict.keys():
+        person_id = filename.split("/")[-1][:6]
+        
         with open(filename) as f:
             soup = BeautifulSoup(f, 'lxml-xml')
-
-        samples = soup.find_all("PANTHOLOGIZATION")
-        person_id = filename.split("/")[-1][:6]
-        for x in samples:
-            print(person_id,";",x)
-
-        continue
+        
 
 
         print(person_id)
@@ -54,19 +50,20 @@ def main():
         birthDeath.extract_death_data(soup, person)
         birthDeath.extract_birth_data(soup, person)
         location.extract_location_data(soup, person)
+        
+        # Not yet reviewed
+        other_contexts.extract_other_contexts_data(soup, person)
 
 
         # cf.extract_cf_data(soup, person)
         # lifeInfo.extract_family_data(soup, person)
         # lifeInfo.extract_intimate_relationships_data(soup, person)
         # lifeInfo.extract_friend_data(soup, person)
-        # other_contexts.extract_other_contexts_data(soup, person)
         # personname.extract_person_name(soup, person)
         # education.extract_education_data(soup, person)
 
         graph = person.to_graph()
         triple_count = len(graph)
-
         if triple_count > highest_triples:
             highest_triples = triple_count
             largest_person = filename
@@ -85,11 +82,17 @@ def main():
     cf.log_mapping_fails()
     occupation.log_mapping_fails()
     organizations.log_mapping()
-    logger.info(str(len(uber_graph)) + " total triples created")
-    logger.info(str(largest_person) + " produces the most triples(" + str(highest_triples) + ")")
-    logger.info(str(smallest_person) + " produces the least triples(" + str(least_triples) + ")")
+    total_triples = len(uber_graph)
+    avg = total_triples / len(file_dict.keys())
+    logger.info(F"{len(file_dict.keys())} files have been converted")
+    logger.info(F"{total_triples} total triples created")
+    logger.info(F"{largest_person} produces the most triples({highest_triples}")
+    logger.info(F"{smallest_person} produces the least triples({least_triples}")
+    logger.info(F"{avg} avg amount of triples per file")
 
-    logger.info("Time completed: " + utilities.get_current_time())
+    logger.info(F"Time completed: {utilities.get_current_time()}")
+
+
 
     temp_path = "extracted_triples/biography_triples.ttl"
     utilities.create_extracted_uberfile(temp_path, uber_graph,serialization="ttl", extra_triples="../data/additional_triples.ttl")
