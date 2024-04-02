02f51725bf0412a285c1693eae6b4601f929d132
Alliyya Mo <alliyyamo.dev@gmail.com>
2024-04-02 00:56:10 -0400

    replace cwrc uri

--- Utils/event.py
+++ Utils/event.py
@@ -214,7 +214,7 @@ class Event(object):
             g.add((self.uri, utilities.NS_DICT["sem"].hasPlace, blanknode))
             g.add((blanknode, RDFS.label, Literal(self.place_str[index])))
             g.add((blanknode, utilities.NS_DICT["cwrc"].hasMappedLocation, place))
-            g.add((blanknode, RDF.type, utilities.NS_DICT["cwrc"].mappedPlace))
+            g.add((blanknode, RDF.type, utilities.NS_DICT["biography"].mappedPlace))
             
 
 
@@ -276,7 +276,7 @@ class Event(object):
             g.add((self.uri, utilities.NS_DICT["sem"].hasPlace, blanknode))
             g.add((blanknode, RDFS.label, Literal(self.place_str[index])))
             g.add((blanknode, utilities.NS_DICT["cwrc"].hasMappedLocation, place))
-            g.add((blanknode, RDF.type, utilities.NS_DICT["cwrc"].mappedPlace))
+            g.add((blanknode, RDF.type, utilities.NS_DICT["biography"].mappedPlace))
             
 
 
